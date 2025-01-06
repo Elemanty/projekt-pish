82738de23bb87bfb4e05e5eb676591351b00e3f0
Elemanty <ei038889@gmail.com>
2025-01-06 18:01:52 +0500

    Update 4. data quality metrics.py

--- 4. data quality metrics.py
+++ 4. data quality metrics.py
@@ -10,8 +10,12 @@ consistency_duplicates = df.duplicated().sum()
 # Актуальность (количество NaN)
 timeliness_nan_count = df.isna().sum()
 
+# Корректность (Чистое изменение свободной энергии не может быть положительным)
+accuracy_value = (df['-5.00'] < 0).mean() * 100
+
 # Вывод метрик
 print(f"Полнота:\n{completeness}\n")
 print(f"Уникальность Gene: {uniqueness_gene:.2f}%\n")
 print(f"Количество дубликатов: {consistency_duplicates}\n")
 print(f"Количество NaN в каждом столбце:\n{timeliness_nan_count}\n")
+print (accuracy_value)
